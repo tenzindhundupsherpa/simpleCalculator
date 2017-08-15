050f0ba4b2736c5dd28c5f41a90c2ddda3e945f7
tenzindhundupsherpa <tenzinnono6@gmail.com>
2017-08-15 18:28:18 +0545

    First calculator

---
+++ calC/src/com/tenzin/calc/Program.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.tenzin.calc;
+
+/**
+ *
+ * @author Home
+ */
+public class Program {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        System.out.println("THIS WILL BE MY FIRST USABLE CALCULATOR");
+                
+                                
+                
+    }
+    
+}
